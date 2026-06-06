a687fb651dd49ba3e510835a6a9471e9e052b824
palwashasheikh <palwashasheikh229@gmail.com>
2026-06-06 18:37:01 +0400

    title icon change

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>
